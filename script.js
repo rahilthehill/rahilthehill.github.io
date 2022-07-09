420c9e038c8c468464cb9134f3d7902a8211e8cb
Rahil Joshi <69742190+rahilthehill@users.noreply.github.com>
2022-07-09 13:32:34 +0800

    website rehaul switch commit

---
+++ script.js
@@ -0,0 +1,97 @@
+(function () {
+  const link = document.querySelectorAll("nav > .hover-this");
+  const cursor = document.querySelector(".cursor");
+
+  const animateit = function (e) {
+    const span = this.querySelector("span");
+    const { offsetX: x, offsetY: y } = e,
+      { offsetWidth: width, offsetHeight: height } = this,
+      move = 25,
+      xMove = (x / width) * (move * 2) - move,
+      yMove = (y / height) * (move * 2) - move;
+
+    span.style.transform = `translate(${xMove}px, ${yMove}px)`;
+
+    if (e.type === "mouseleave") span.style.transform = "";
+  };
+
+  const editCursor = (e) => {
+    const { clientX: x, clientY: y } = e;
+    cursor.style.left = x + "px";
+    cursor.style.top = y + "px";
+  };
+
+  link.forEach((b) => b.addEventListener("mousemove", animateit));
+  link.forEach((b) => b.addEventListener("mouseleave", animateit));
+  window.addEventListener("mousemove", editCursor);
+})();
+
+const button = document.getElementById("b");
+const items = document.querySelector(".body__container");
+
+buttonHandler = () => {
+  // basically copying the html
+  const item = document.createElement("div");
+  item.classList.add("item");
+  item.innerHTML =
+    `
+    <<div class="cursor"></div>
+    ` >
+    //adding a new box to the class "body__container"
+    items.appendChild(item);
+};
+
+// need an event listener on the button
+button.addEventListener("click", buttonHandler);
+
+var words = [
+    "Welcome to Rahilj",
+    "This is where i share",
+    "Projects",
+    "Creations",
+    "And much more",
+    "So Click to start!",
+  ],
+  part,
+  i = 0,
+  offset = 0,
+  len = words.length,
+  forwards = true,
+  skip_count = 0,
+  skip_delay = 15,
+  speed = 70;
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    } else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      } else {
+        offset--;
+      }
+    }
+    $(".word").text(part);
+  }, speed);
+};
+
+$(document).ready(function () {
+  wordflick();
+});
